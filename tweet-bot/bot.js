c7afea3d6c2d598b55ae126e4db4a0407c7366e9
travisen <travis.nykaza@gmail.com>
2016-05-20 00:57:53 -0400

    added functionality to parse out specific attributes from returned JSON object

--- tweet-bot/bot.js
+++ tweet-bot/bot.js
@@ -1,9 +1,17 @@
-//Callback functions used for logging errors
+var express = require('express');
+
+var router = express.Router();
+    //Callback functions
+    // used for logging errors
     var error = function (err, response, body) {
         console.log('ERROR [%s]', err);
     };
+    // used for recieving data
     var success = function (data) {
         console.log('Data from our call[%s]', data);
+        var response = JSON.parse(data); // response stores an object containing the data recieved
+        console.log("This is the tweet's contents:", response.text,
+          response.id_str, response.user.screen_name);
     };
 
     var Twitter = require('twitter-node-client').Twitter;
@@ -21,9 +29,23 @@
 
     var twitter = new Twitter(config);
 
-    twitter.getTweet({ id: '732595450746970112'}, error, success);
+    twitter.getTweet({ id: '732595450746970112'}, error,
+     success);
 
+    router.get('/', function(req, res, next) {
+  // https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+  client.get('statuses/user_timeline', { screen_name: 'nodejs', count: 20 },
+   function(error, tweets, response) {
+    if (!error) {
+      res.status(200).render('index', { title: 'Express', tweets: tweets });
+    }
+    else {
+      res.status(500).json({ error: error });
+      }
+    });
+  });
 
+  module.exports = router;
     //Example calls
 
     //twitter.getUserTimeline({ screen_name: 'theAbrasiveBot', count: '1'}, error, success);
